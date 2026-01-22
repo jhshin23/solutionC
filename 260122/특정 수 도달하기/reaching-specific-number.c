03bcf1815ec827fc8fae2f368f66078219d61ba2
jhshin23 <180617768+jhshin23@users.noreply.github.com>
2026-01-22 11:19:50 +0900

    Title: 특정 수 도달하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260122/특정 수 도달하기/reaching-specific-number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define SIZE 10
+int main() {
+    // Please write your code here.
+    int buffer[SIZE];
+    int sum = 0;
+    double avrg = 0;
+    for (int i = 0; i < SIZE; i++) {
+        scanf(" %d", &buffer[i]);
+    }
+    for (int i = 0; i < SIZE; i++) {
+        if (buffer[i] < 250) sum += buffer[i];
+        else {
+            avrg = (double)sum / i;
+            break;
+        }
+    }    
+    printf("%d %.1f", sum, avrg);
+    return 0;
+}
